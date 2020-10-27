db7be3f4f17eeee428e9ef6ce015dbed143f355e
Alberto Liguori <a1liguorialberto@gmail.com>
2020-10-27 15:11:23 +0100

    Pali Done!

---
+++ js-pali/script.js
@@ -0,0 +1,47 @@
+//
+// document.getElementById("go").addEventListener("click", function(){
+//
+//   var word = document.getElementById("palInput").value.toLowerCase()
+//
+//   //.split trasforma una stringa in un array. Se ARG = ("") inserirà
+//   // ogni lettera che compone la stringa come elementi dell'Array
+//   var wordToArray = word.split("")
+//   console.log(wordToArray)
+//
+//   //.reverse inverte l'ordine degli elementi di un array
+//   var reversedArray= wordToArray.reverse()
+//   console.log(reversedArray)
+//
+//   //.join trasforma gli elementi di un array in una stringa.Se mancano le virgolette
+//   // restituirà ogni elemento separato dalla virgola
+//   var reversedArrayToString = reversedArray.join("")
+//
+//   console.log(reversedArrayToString)
+//
+//   if(reversedArrayToString == word){
+//     document.getElementById("result").innerHTML="La parola inserita è Palindroma"
+//   }else{
+//     document.getElementById("result").innerHTML="La parola inserita <strong> NON </strong> è Palindroma"
+//   }
+//
+// })
+
+
+function reverseWord (word){
+  var wordToArray = word.split("");
+  var reversedArray= wordToArray.reverse();
+  var reversedArrayToString = reversedArray.join("");
+  return (reversedArrayToString == word)
+}
+
+document.getElementById("go").addEventListener("click", function(){
+
+var inputWord = document.getElementById("palInput").value.toLowerCase()
+
+if (reverseWord(inputWord)){
+  document.getElementById("result").innerHTML="La parola inserita è Palindroma"
+}else{
+  document.getElementById("result").innerHTML="La parola inserita <strong> NON </strong> è Palindroma"
+  }
+
+})
